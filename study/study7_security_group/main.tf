39de91a2107d357187290ee918bbe2fafcb0b396
Junichi Koyama <koyama@rightcode.co.jp>
2021-03-12 16:16:08 +0900

    fix

--- study/study7_security_group/main.tf
+++ study/study7_security_group/main.tf
@@ -1,7 +1,13 @@
+# リージョンの明示的な定義
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
 # EC2の構築
 resource "aws_instance" "study7" {
   ami           = "ami-0f9ae750e8274075b"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.study7.id]
 
   # Apacheのインストール
   user_data = <<EOF
@@ -10,10 +16,7 @@ resource "aws_instance" "study7" {
     systemctl start httpd.service
 EOF
 }
-# リージョンの明示的な定義
-provider "aws" {
-    region = "ap-northeast-1"
-}
+
 # セキュリティグループの定義
 resource "aws_security_group" "study7"{
     # study7 というセキュリティグループを作成
@@ -34,4 +37,8 @@ resource "aws_security_group" "study7"{
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "study7_publci_dns" {
+    value = aws_instance.study7.public_dns
 }
